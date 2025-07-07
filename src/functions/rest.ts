2a5ded8bf6eb9d7ad48c452e3251817a22161bef
Kayky <kaykyribeiro207@gmail.com>
2025-07-07 16:10:10 -0300

    feat: adicionar função utilizando o operador rest

---
+++ src/functions/rest.ts
@@ -0,0 +1,31 @@
+/* function totalVendas(venda1: number, venda2: number, venda3: number, venda4:number){
+    const total = venda1 + venda2 + venda3 + venda4;
+
+    console.log(total)
+
+    return total
+}
+
+totalVendas(10, 30, 50, 10) */
+
+/* function totalVendas(...vendas: number[]):void{
+    const quantidadeVendas = vendas.length;
+
+    console.log(`Você fez ${quantidadeVendas} vendas hoje!`)
+}
+
+totalVendas(10, 30, 25); */
+
+function soma(...numeros: number[]): number {
+    return numeros.reduce((acc, num) => acc + num, 0);
+}
+
+console.log(soma(8, 5, 7))
+
+// o rest operator (...) é usado para agrupar um número indefinido de argumentos em um array.
+// O rest operator deve ser o último parâmetro da função.
+
+// o retorno do reduce é o valor acumulado, que é atualizado a cada iteração do array.
+
+// O reduce recebe dois parâmetros: o primeiro é uma função que recebe o acumulador e o
+// valor atual, e o segundo é o valor inicial do acumulador (neste caso, 0).
